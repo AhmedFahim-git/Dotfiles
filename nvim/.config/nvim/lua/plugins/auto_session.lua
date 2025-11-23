2db32ac8fd8f8d9725429e5adb79b24a9e4fd594
Ahmed Fahim <afahim03@yahoo.com>
2025-11-24 00:39:00 +0600

    update neovim config

--- nvim/.config/nvim/lua/plugins/auto_session.lua
+++ nvim/.config/nvim/lua/plugins/auto_session.lua
@@ -14,7 +14,7 @@ return {
         vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
         require("auto-session").setup({
             pre_save_cmds = { "Neotree close" },
-            post_restore_cmds = { "Neotree filesystem show" },
+            -- post_restore_cmds = { "Neotree filesystem show" },
         })
     end,
 }
